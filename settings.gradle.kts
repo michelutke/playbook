09bb0c752c2c842270415165b34a0d4d834f8ebf
Miggi <miggi@playbook.com>
2026-03-12 18:35:38 +0000

    fix: add JetBrains dev repositories for alpha artifacts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,6 +12,8 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/interactive")
     }
 }
 
