ed0641cd6d089c981743e222cbdd1c6d00ac6056
Michel Utke <utke.michel@gmail.com>
2026-03-19 12:03:45 +0100

    feat(03-03): shared KMP domain models + EventRepository interface
    - Add Event, EventWithTeams, MatchedTeam data classes with @Serializable
    - Add CreateEventRequest, EditEventRequest, RecurringPattern, SubGroup data classes
    - Add EventType enum with companion fromValue
    - Use kotlinx.datetime.Instant (not java.time) for KMP compatibility
    - Add EventRepository interface with Result<T> return types
    - Add TODO comment in SharedModule for Plan 04 impl registration

--- shared/src/commonMain/kotlin/ch/teamorg/di/SharedModule.kt
+++ shared/src/commonMain/kotlin/ch/teamorg/di/SharedModule.kt
@@ -3,3 +3,5 @@ package ch.teamorg.di
 import org.koin.core.module.Module
 
 expect val sharedModule: Module
+
+// TODO Plan 04: single<EventRepository> { EventRepositoryImpl(get()) }
---
+++ shared/src/commonMain/kotlin/ch/teamorg/repository/EventRepository.kt
@@ -0,0 +1,28 @@
+package ch.teamorg.repository
+
+import ch.teamorg.domain.CreateEventRequest
+import ch.teamorg.domain.EditEventRequest
+import ch.teamorg.domain.Event
+import ch.teamorg.domain.EventWithTeams
+import ch.teamorg.domain.SubGroup
+
+interface EventRepository {
+    suspend fun getMyEvents(
+        from: String? = null,
+        to: String? = null,
+        type: String? = null,
+        teamId: String? = null
+    ): Result<List<EventWithTeams>>
+
+    suspend fun getEventDetail(id: String): Result<EventWithTeams>
+
+    suspend fun createEvent(request: CreateEventRequest): Result<Event>
+
+    suspend fun editEvent(id: String, request: EditEventRequest): Result<Event>
+
+    suspend fun cancelEvent(id: String, scope: String = "this_only"): Result<Unit>
+
+    suspend fun duplicateEvent(id: String): Result<Event>
+
+    suspend fun getSubGroups(teamId: String): Result<List<SubGroup>>
+}
