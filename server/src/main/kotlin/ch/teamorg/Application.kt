9998c3c6cddf488165463d34b479c9ee0cf81361
Michel Utke <utke.michel@gmail.com>
2026-03-19 12:08:19 +0100

    feat(03-02): materialisation background job + Application wiring
    - EventMaterialisationJob.kt: runs materialiseUpcomingOccurrences() on startup then every 24h
    - Uses Dispatchers.IO, catches exceptions to prevent server crash
    - Application.kt: calls startMaterialisationJob() after configureRouting()

--- server/src/main/kotlin/ch/teamorg/Application.kt
+++ server/src/main/kotlin/ch/teamorg/Application.kt
@@ -1,6 +1,7 @@
 package ch.teamorg
 
 import ch.teamorg.infra.DatabaseFactory
+import ch.teamorg.infra.startMaterialisationJob
 import ch.teamorg.plugins.*
 import io.ktor.server.application.*
 
@@ -14,4 +15,5 @@ fun Application.module() {
     configureStaticFiles()
     configureAuth()
     configureRouting()
+    startMaterialisationJob()
 }
